aceed8f380822921520d8d6a15896f178505aca1
HansMarcus01 <hanzel.yael@hotmail.com>
2026-05-30 21:38:16 -0600

    Feat: adding tringger instead of for the table historico_status_laptop

---
+++ code/s-06-ilap-trigger-historico_status_laptop.sql
@@ -0,0 +1,45 @@
+--@Autor: Hansel Yael Tepal Briseño
+--@Fecha creación: 24/05/2026
+--@Descripción: Definición del trigger instead of para la vista historico_status_laptop
+
+create or replace trigger t_dml_historico_status_laptop
+instead of insert or update or delete on historico_status_laptop
+begin
+    case
+        when inserting then
+            if :new.fecha_status < to_date('01-01-2010','dd-mm-yyyy') then
+                insert into historico_status_laptop_f1 (historico_status_laptop_id,
+                    fecha_status, laptop_id, status_laptop_id)
+                values (historico_status_laptop_seq.nextval, :new.fecha_status,
+                    :new.laptop_id, :new.status_laptop_id);
+            elsif :new.fecha_status >= to_date('01-01-2010','dd-mm-yyyy') then
+                insert into historico_status_laptop_f2 (historico_status_laptop_id,
+                    fecha_status, laptop_id, status_laptop_id)
+                values (historico_status_laptop_seq.nextval, :new.fecha_status,
+                    :new.laptop_id, :new.status_laptop_id);
+            else
+                raise_application_error(-20010,
+                    'El registro con fecha_status '
+                    || to_char(:new.fecha_status,'dd-mm-yyyy')
+                    || ' no cumple con las fechas permitidas para los fragmentos.');
+            end if;
+        when updating then
+            raise_application_error(-20003, 'No se permiten eliminaciones en
+                la vista historico_status_laptop');
+        when deleting then
+            if :old.fecha_status < to_date('01-01-2010','dd-mm-yyyy') then
+                delete from historico_status_laptop_f1
+                where historico_status_laptop_id = :old.historico_status_laptop_id;
+            elsif :old.fecha_status >= to_date('01-01-2010','dd-mm-yyyy') then
+                delete from historico_status_laptop_f2
+                where historico_status_laptop_id = :old.historico_status_laptop_id;
+            else
+                raise_application_error(-20010,
+                    'El registro con fecha_status '
+                    || to_char(:old.fecha_status,'dd-mm-yyyy')
+                    || 'que se desea eliminar no cumple con las fechas permitidas
+                        para los fragmentos.');
+            end if;
+    end case;
+end;
+/
